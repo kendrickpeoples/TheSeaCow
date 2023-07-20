9fad9961c3c9ccbe66842e4d074dd35012e07d92
kendrickpeoples <69645658+kendrickpeoples@users.noreply.github.com>
2023-07-19 20:51:43 -0500

    added action and method to form and php for mail.

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    $mailTo = "kendrickpeoples07@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received a message from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsend");
+}
